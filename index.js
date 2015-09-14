009df65c366d4fd9ab717aa0e15d32d833d7485f
Stéphane D <derste@gmail.com>
2015-09-15 01:43:17 +0200

    Use ES5 import to avoid babelize

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-export default function oneliner(str) {
+module.exports = function oneliner(str) {
   return str.replace(/[\r\n\t ]+/g, ' ');
-}
-
-
-const t = oneliner(`Hey !
-This is my text file
-  with a lot of blabla and some code:
-    function t(a, b) {
-      return a + b;
-    }`);
-
-console.log(t);
+};
